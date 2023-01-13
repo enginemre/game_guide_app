9c064859f90911c6033b300fab865cd44e21f6ae
enginemre <engiinemre@gmail.com>
2023-01-13 14:51:22 +0300

    initialize project

---
+++ game_guide_app/Common/Components/MyView.swift
@@ -0,0 +1,20 @@
+//
+//  MyView.swift
+//  game_guide_app
+//
+//  Created by Emre Muhammet Engin on 13.01.2023.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
